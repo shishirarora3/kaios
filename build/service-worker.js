ef5fc67a7cc703553ea171ba2fc2d6d4567024a4
shishirarora3 <shiarora@microsoft.com>
2020-05-26 08:56:34 +0530

    adding KaiUI library and doing tabing

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf83714864c27173b1b6e4d7061ba45a.js"
+  "/precache-manifest.c541f3a47c03498701944eeed5dae0de.js"
 );
 
 self.addEventListener('message', (event) => {
