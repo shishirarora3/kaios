6f111cbaca1587e8f0c180405f2d4c9b89636487
shishirarora3 <shiarora@microsoft.com>
2020-05-29 20:07:02 +0530

    gh-pages

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,10 @@
 module.exports = {
   appId: '98f6c11f-26a7-4309-a75f-b8df6e384711',
-  redirectUri: process.env.NODE_ENV === 'development' ||
-  process.env.REACT_APP_KAI_UI ||
-      process.env.REACT_APP_DEMO
+  redirectUri:
+      process.env.NODE_ENV === 'development' ||
+      process.env.REACT_APP_KAI_UI ||
+      process.env.REACT_APP_DEMO ||
+      process.env.REACT_APP_INTEGRATED_APP
       ? window.location.href: 'https://outlook-sdf.office.com/mail/inbox',
   scopes: [
     'user.read',
