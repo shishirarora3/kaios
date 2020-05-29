abab6244722ec89aac0fa9f572e5c032bfbe0d07
shishirarora3 <shiarora@microsoft.com>
2020-05-29 19:21:36 +0530

    gh-pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import 'react-app-polyfill/stable'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import KaiUI from './kai-ui/src/App';
+
 const bootstrap = () => {
     // Render the app
     console.log(process.env.REACT_APP_DEMO);
-    if(process.env.REACT_APP_DEMO){
-        ReactDOM.render(<div>Test</div>, document.getElementById('root'));
-    }else{
+    if(process.env.REACT_APP_KAI_UI){
+        ReactDOM.render(<KaiUI/>, document.getElementById('root'));
+    }else if(process.env.REACT_APP_DEMO){
         ReactDOM.render(<App />, document.getElementById('root'));
     }
 };
