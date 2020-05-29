7105b13b851bda9032e52568177f42a304d4b071
shishirarora3 <shiarora@microsoft.com>
2020-05-29 19:17:28 +0530

    gh-pages

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 const bootstrap = () => {
     // Render the app
-    console.log(process.env.DEMO);
-    if(process.env.DEMO){
-        ReactDOM.render(<div>true</div>, document.getElementById('root'));
+    console.log(process.env.REACT_APP_DEMO);
+    if(process.env.REACT_APP_DEMO){
+        ReactDOM.render(<div>Test</div>, document.getElementById('root'));
     }else{
         ReactDOM.render(<App />, document.getElementById('root'));
     }
