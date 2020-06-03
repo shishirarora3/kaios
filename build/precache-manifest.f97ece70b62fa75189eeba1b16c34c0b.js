e77455d016658627301cd4f88ae8e8a7effd7945
prmeher <prmeherkaur@gmail.com>
2020-06-03 17:37:44 +0530

    Design changes made according to Firefox Simulator

---
+++ build/precache-manifest.f97ece70b62fa75189eeba1b16c34c0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6bd9f8cde07024e1079455970750e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d33ad42afc65b689242",
+    "url": "/static/css/main.416dd531.css"
+  },
+  {
+    "revision": "6d33ad42afc65b689242",
+    "url": "/static/js/main.36812770.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/main.36812770.js.LICENSE.txt"
+  },
+  {
+    "revision": "07127065df308060d7243d0d914b0819",
+    "url": "/static/media/kai-icons.07127065.svg"
+  },
+  {
+    "revision": "4e2f66190ab901acc056c5aa6d1cac58",
+    "url": "/static/media/kai-icons.4e2f6619.woff"
+  },
+  {
+    "revision": "5a1c5c18aca6765abadea46336312509",
+    "url": "/static/media/kai-icons.5a1c5c18.ttf"
+  },
+  {
+    "revision": "686d74dfef715329736523da67a68ce0",
+    "url": "/static/media/kai-icons.686d74df.eot"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kaios/precache-manifest.5ba47276f4db08f26393fa9fb36987bf.js"
+  "/precache-manifest.f97ece70b62fa75189eeba1b16c34c0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kaios/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
