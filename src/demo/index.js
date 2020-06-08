933f595c4faa3aa0ba97202120fea98f6e973809
shishirarora3 <shiarora@microsoft.com>
2020-06-08 22:24:14 +0530

    adding support for scss adding poc for using scss variables.scss variables in javascript (css in js) removing build folder from repo

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {News} from "../news/News";
 
 const App=()=><pre>
+    <News/>
     This is demo. use this to make demo actual components using KaiUI and designs given
 </pre>
 export default App;
