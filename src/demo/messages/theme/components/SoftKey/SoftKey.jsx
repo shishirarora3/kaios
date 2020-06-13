8f0dcfab25be73857db3401c0d628c86b99dc961
dev_gupta01 <devashishgupta13022000@gmail.com>
2020-06-13 19:29:59 +0530

    typeramp screen added

--- src/demo/messages/theme/components/SoftKey/SoftKey.jsx
+++ src/demo/messages/theme/components/SoftKey/SoftKey.jsx
@@ -6,7 +6,7 @@ import './SoftKey.scss';
 const prefixCls = 'kai-softkeyD';
 
 const Button = props => {
-  const { handleClick, icon, text, theme, color } = props;
+  const { handleClick, icon, text, theme, color,type } = props;
 
   const handleButtonClick = e => {
     e.preventDefault();
@@ -27,10 +27,10 @@ const Button = props => {
 
   const themeCls= `kai-softkey-${theme}`;
   const colorCls= `kai-softkey-${color}`;
-
+  const typeCls=`kai-softkey-${type}`
   return (
     <button
-      className={`${prefixCls}-btn ${themeCls}-btn ${colorCls}-btn`}
+      className={`${prefixCls}-btn ${themeCls}-btn ${colorCls}-btn ${typeCls}`}
       onClick={handleButtonClick}
       onFocus={handleCheckFocus}
     >
@@ -50,12 +50,12 @@ const SoftKey = React.memo(props => {
     centerText,
     centerIcon,
     theme,
-    color
+    color,
+    type
   } = props;
 
   const themeCls= `kai-softkey-${theme}`;
-  const colorCls= `kai-softkey-${color}`;
-
+  const typeCls=`kai-softkey-${type}`;
   const handleKeyDown = useCallback(
     e => {
       switch (e.key) {
@@ -85,8 +85,8 @@ const SoftKey = React.memo(props => {
   }, [handleKeyDown]);
 
   return (
-    <div className={`${prefixCls} visible ${themeCls}`}>
-      <Button pos="left" text={leftText} handleClick={leftCallback} theme={theme} color={color}/>
+    <div className={`${prefixCls} visible ${themeCls} ${typeCls}`}>
+      <Button pos="left" text={leftText} handleClick={leftCallback} theme={theme} color={color} type={type}/>
       <Button
         pos="center"
         text={centerText}
@@ -94,8 +94,9 @@ const SoftKey = React.memo(props => {
         handleClick={centerCallback}
         theme={theme}
         color={color}
+        type={type}
       />
-      <Button pos="right" text={rightText} handleClick={rightCallback} theme={theme} color={color}/>
+      <Button pos="right" text={rightText} handleClick={rightCallback} theme={theme} color={color} type={type}/>
     </div>
   );
 });
@@ -109,7 +110,8 @@ SoftKey.propTypes = {
   centerCallback: PropTypes.func,
   rightCallback: PropTypes.func,
   theme: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  type: PropTypes.string
 };
 
 SoftKey.defaultProps = {
@@ -121,7 +123,8 @@ SoftKey.defaultProps = {
   centerCallback: () => {},
   rightCallback: () => {},
   theme: "white",
-  color: "monochrome"
+  color: "monochrome",
+  type: "default"
 };
 
 export default SoftKey;
